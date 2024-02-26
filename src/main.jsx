e4a852163f800455b6744f28880be0ba6e0e8487
Radu Baltag <baltag.radu@gmail.com>
2024-02-26 12:38:19 +0100

    using redux persist for storing items to localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import App from './components/App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './stores/reduxStore.js';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/stores/reduxStore.js
+++ src/stores/reduxStore.js
@@ -1,6 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import itemsReducer from './itemsSlice';
 
+const persistConfig = {
+  key: 'items-storage-redux',
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({ items: itemsReducer });
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: { items: itemsReducer },
+  reducer: persistedReducer,
 });
